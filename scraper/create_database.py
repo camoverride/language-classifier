274f1ac2d3a87db65450a23681370697a58d06db
camoverride <camoverride@gmail.com>
2018-01-10 13:32:40 -0800

    tweaks

--- scraper/create_database.py
+++ scraper/create_database.py
@@ -29,95 +29,69 @@ class DataBaseWriter(object):
         self.db_root_location = db_root_location
 
 
+    def _count_words_in_language(self, language):
+        """
+        Counts the number of words in all the .txt files for a single language.
+        """
+        words = 0
+        language_data_folder = join(self.db_root_location, language)
+        contents = listdir(language_data_folder)
+        articles = [article for article in contents if article[-3:] == 'txt']
+        for article in articles:
+            with open(join(language_data_folder, article), 'r') as text:
+                for line in text:
+                    for _ in line.split(' '):
+                        words += 1
+
+        return words
+
+
+    def _get_words_in_language(self, language):
+        """
+        Iterates through all the articles downloaded for a single language, collecting all the words
+        by splitting on whitespace and returns the list of words.
+        """
+        words = []
+        language_data_folder = join(self.db_root_location, language)
+        contents = listdir(language_data_folder)
+        articles = [article for article in contents if article[-3:] == 'txt']
+        for article in articles:
+            with open(join(language_data_folder, article), 'r') as text:
+                for line in text:
+                    for word in line.split(' '):
+                        words.append(word)
+
+        return words
+
+        
     def dbwrite(self, language, words, exact_words=False):
         """
         Given a language, this function downloads 25 Wikipedia articles. It then reads those
         articles and adds words to an "all_articles.dat" file until the number of words is
         reached. It also leaves behind the articles downloaded as .txt files.
         """
-        # debug output
+        # Debug output.
         print('checking ' + language + ' articles')
 
         language_data_folder = join(self.db_root_location, language)
-        language_data_db = join(self.db_root_location, language, 'all_articles.dat')
+        train_ratio = 0.9
 
         # Checks if the folder for the specific language exists. Creates it if it doesn't.
         if language not in listdir(self.db_root_location):
             subprocess.run(["mkdir", language_data_folder])
 
-        # Checks if the database file exists. Creates it if it doesn't.
-        if 'all_articles.dat' not in language_data_folder:
-            subprocess.run(["touch", language_data_db])
+        # If there are not enough words downloaded, download more.
+        while self._count_words_in_language(language) < words:
+            self.getdata.write_articles(language, 25, language_data_folder)
 
-        # Counts lines in all_articles.db, the database file.
-        words_in_db = 0
-        with open(language_data_db) as articles:
-            for line in articles:
-                for word in line.split():
-                    words_in_db += 1
-        articles.close()
+        # Read all the words into a list, removing any extras. Divide into training and test sets.
+        all_words = self._get_words_in_language(language)[:words]
+        split = int(train_ratio * words)
+        training_set = all_words[split:]
+        test_set = all_words[:split]
 
-        # debug
-        print('   > Target words:', words)
-        print('   > Current words:', words_in_db)
-
-        # Checks whether enough words are in the database.
-        if words_in_db < words:
-            words_in_db = 0
-
-            # While there are not enough words, download more.
-            while words_in_db < words:
-
-                # Set to zero, because all words are counted every time.
-                words_in_db = 0
-                self.getdata.write_articles(language, 25, language_data_folder)
-
-                # Overrides database file.
-                with open(language_data_db, 'w') as db:
-
-                    # Gathers up all the Wikipedia files.
-                    contents = listdir(language_data_folder)
-                    articles = [article for article in contents if article[-3:] == 'txt']
-
-                    # Iterates through the Wiki files, adding their words to the database.
-                    for article in articles:
-                        with open(join(language_data_folder, article)) as a:
-                            for line in a:
-                                for word in line.split():
-                                    words_in_db += 1
-                                    db.write(word)
-                                    db.write(' ')
-
-                                    # The loop is terminated if an exact word count is desired.
-                                    if exact_words and words_in_db >= words:
-                                        break
-                                else:
-                                    continue
-                                break
-                            else:
-                                continue
-                            break
-
-                # debug
-                print('   ... adding words. Total:', words_in_db)
-
-        # splits all_data into training and test sets
-        train = int(0.9 * words_in_db)
-        all_words = []
-        with open(language_data_db) as database:
-            for line in database:
-                for word in line.split():
-                    all_words.append(word)
-
-        with open(join(language_data_folder, 'TRAINING_SET.db'), 'w') as training_set:
-            for word in all_words[:train]:
-                training_set.write(word)
-                training_set.write(' ')
-
-        with open(join(language_data_folder, 'TEST_SET.db'), 'w') as test_set:
-            for word in all_words[train:]:
-                test_set.write(word)
-                test_set.write(' ')
+        # Write these sets to the SQL database.
+        
 
 
 if __name__ == "__main__":
